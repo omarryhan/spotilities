ce5f7a33d1725a18fa94c997b11169312818b568
Omar Ryhan <omarryhan@gmail.com>
2020-05-31 17:46:00 +0200

    New logo to comply with Spotify's branding guidlines

--- configs/theme.ts
+++ configs/theme.ts
@@ -2,3 +2,20 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 export const styledComponentTheme = createMuiTheme({
 });
+
+export const colors = {
+  gray: {
+    navbar: 'rgba(40, 40, 40, 1)',
+    background: 'rgba(18, 18, 18, 1)',
+    darkest: 'rgba(4, 4, 4, 1)',
+    default: 'rgba(25, 20, 20, 1)',
+  },
+  green: {
+    secondary: 'rgba(40, 108, 71, 1)',
+    default: '',
+    defaultt: 'rgba(29, 185, 84, 1)',
+  },
+  white: {
+    default: 'rgba(255, 255, 255, 1)',
+  },
+};
