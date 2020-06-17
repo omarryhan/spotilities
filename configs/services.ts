17d38515d9c85ec5441fc4abb1605b7f3bfeac22
Omar Ryhan <omarryhan@gmail.com>
2020-06-17 15:46:31 +0200

    Removed unneeded permissions

--- configs/services.ts
+++ configs/services.ts
@@ -10,17 +10,9 @@ export const GA_CONFIGS: ReactGA.InitializeOptions = {
 export const SPOTIFY_CLIENT_ID = '5334d5bcc697492eb8b13846199b734b';
 
 export const SPOTIFY_SCOPES_NEEDED = [
-  'user-read-private',
-  'user-read-email',
   'playlist-read-collaborative',
-  'playlist-modify-public',
   'playlist-read-private',
-  'playlist-modify-private',
   'user-library-read',
-  'user-library-modify',
   'user-top-read',
-  'user-read-recently-played',
-  'user-follow-read',
-  'user-read-playback-state',
   'user-modify-playback-state',
 ];
